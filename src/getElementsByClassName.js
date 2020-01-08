28334486d266acbb889df042a5242b05dfb08744
gentashibasaki <genta.shibasaki.78@gmail.com>
2020-01-08 15:55:01 +0900

    Implemented getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,19 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (name) => {
+  const result = [];
+  function matchNode(node) {
+    if (node.classList && node.classList.contains(name)) {
+      result.push(node);
+    }
+    const nodeElement = node.childNodes;
+    for (let i = 0; i < nodeElement.length; i++) {
+      matchNode(nodeElement[i]);
+      console.log(nodeElement.length);
+      // return result;
+    }
+  }
+  matchNode(document.body);
+  console.log(result);
+  return result;
 };
 
 module.exports = { getElementsByClassName };
